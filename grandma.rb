1b8d45a613417650e7587e59e8711d8310e8c3cc
Christopher Dent <christopherdent01@gmail.com>
2019-07-17 01:11:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,8 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
     elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else return "HUH?! SPEAK UP, SONNY!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
