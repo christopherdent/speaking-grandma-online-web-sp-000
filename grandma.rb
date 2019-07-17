b9dc270191f4fc9f491ed5e68b7a814e02a45ad5
Christopher Dent <christopherdent01@gmail.com>
2019-07-17 01:08:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,12 +14,13 @@
 
 
 def speak_to_grandma(phrase)
-  if !phrase.upcase
-    puts "HUH?! SPEAK UP, SONNY"
-  elseif phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  else
+    elsif phrase.upcase?
     puts "NO, NOT SINCE 1938!"
+  else puts
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
-    
+
+  
